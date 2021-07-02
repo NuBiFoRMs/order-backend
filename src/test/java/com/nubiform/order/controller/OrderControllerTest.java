555b5d1d701bff732d0cf6db8a60d356cd1693c8
NuBiFoRM <nubiform@me.com>
2021-07-03 00:24:54 +0900

    create order test

--- src/test/java/com/nubiform/order/controller/OrderControllerTest.java
+++ src/test/java/com/nubiform/order/controller/OrderControllerTest.java
@@ -1,9 +1,11 @@
 package com.nubiform.order.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nubiform.order.domain.Member;
 import com.nubiform.order.domain.Order;
 import com.nubiform.order.repository.MemberRepository;
 import com.nubiform.order.repository.OrderRepository;
+import com.nubiform.order.vo.request.OrderRequest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -34,6 +36,9 @@ class OrderControllerTest {
 
     @Autowired
     OrderRepository orderRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
@@ -85,9 +90,26 @@ class OrderControllerTest {
     }
 
     @Test
-    public void orderTest() throws Exception {
-        mockMvc.perform(post(API_V1_ORDERS_URI)
-                .contentType(MediaType.APPLICATION_JSON))
+    public void orderByNicknameTest() throws Exception {
+        OrderRequest orderRequest = new OrderRequest();
+        orderRequest.setProduct("newProduct");
+
+        mockMvc.perform(post(API_V1_ORDERS_URI + PATH_VARIABLE_USER_ID, "nickname")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(orderRequest)))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void orderByEmailTest() throws Exception {
+        OrderRequest orderRequest = new OrderRequest();
+        orderRequest.setProduct("newProduct");
+
+        mockMvc.perform(post(API_V1_ORDERS_URI + PATH_VARIABLE_USER_ID, "email")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(orderRequest)))
+                .andDo(print())
                 .andExpect(status().isOk());
     }
 }
