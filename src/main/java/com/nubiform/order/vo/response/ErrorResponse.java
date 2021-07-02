2df6b89b746a7230e47e98230465ca693498a00a
NuBiFoRM <nubiform@me.com>
2021-07-02 10:45:44 +0900

    error response

---
+++ src/main/java/com/nubiform/order/vo/response/ErrorResponse.java
@@ -0,0 +1,23 @@
+package com.nubiform.order.vo.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.nubiform.order.constant.ApiError;
+import lombok.Data;
+
+@Data
+public class ErrorResponse<T> {
+
+    private ApiError apiError;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T details;
+
+    public ErrorResponse(ApiError apiError, String message, T details) {
+        this.apiError = apiError;
+        this.message = message;
+        this.details = details;
+    }
+}
