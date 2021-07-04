28d61e789955f67e6abed11a58984d340769cb5f
NuBiFoRM <nubiform@me.com>
2021-07-04 16:10:15 +0900

    secret key

---
+++ src/test/java/com/nubiform/order/config/security/jwt/JwtPropertiesTest.java
@@ -0,0 +1,32 @@
+package com.nubiform.order.config.security.jwt;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class JwtPropertiesTest {
+
+    public static final String SECRET = "order-backend-8a79f833-6c60-4c3e-a5a5-6420136e1861";
+
+    @Autowired
+    JwtProperties jwtProperties;
+
+    @Test
+    public void encodePropertyTest() {
+        String secret = Encoders.BASE64.encode(SECRET.getBytes(StandardCharsets.UTF_8));
+        assertThat(secret).isEqualTo(jwtProperties.getSecret());
+    }
+
+    @Test
+    public void decodePropertyTest() {
+        String secret = new String(Decoders.BASE64.decode(jwtProperties.getSecret()));
+        assertThat(secret).isEqualTo(SECRET);
+    }
+}
