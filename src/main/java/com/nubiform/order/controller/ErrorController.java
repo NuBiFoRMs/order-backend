a0190d2c8f0a6e21007df570074e1a70ad289192
NuBiFoRM <nubiform@me.com>
2021-07-04 18:50:13 +0900

    test code

--- src/main/java/com/nubiform/order/controller/ErrorController.java
+++ src/main/java/com/nubiform/order/controller/ErrorController.java
@@ -4,17 +4,22 @@ import com.nubiform.order.constant.ApiError;
 import com.nubiform.order.exception.ApiException;
 import com.nubiform.order.exception.ApiParameterException;
 import com.nubiform.order.vo.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 
+@Slf4j
 @RestControllerAdvice
 public class ErrorController {
 
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ErrorResponse> apiException(ApiException e) {
+        log.error("apiException: {}", e.getLocalizedMessage());
         return ResponseEntity
                 .badRequest()
                 .body(new ErrorResponse(e.getApiError(), e.getLocalizedMessage()));
@@ -22,13 +27,23 @@ public class ErrorController {
 
     @ExceptionHandler(ApiParameterException.class)
     public ResponseEntity<ErrorResponse<List<ApiParameterException.ErrorInfo>>> apiParameterException(ApiParameterException e) {
+        log.error("apiParameterException: {}", e.getLocalizedMessage());
         return ResponseEntity
                 .badRequest()
                 .body(new ErrorResponse<>(e.getApiError(), e.getLocalizedMessage(), e.getFields()));
     }
 
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponse> badCredentialsException(BadCredentialsException e) {
+        log.error("badCredentialsException: {}", e.getLocalizedMessage());
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponse(ApiError.UNAUTHORIZED, e.getLocalizedMessage()));
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> exception(Exception e) {
+        log.error("exception: {}, {}", e.getClass(), e.getLocalizedMessage());
         return ResponseEntity
                 .internalServerError()
                 .body(new ErrorResponse(ApiError.ERROR, e.getLocalizedMessage()));
