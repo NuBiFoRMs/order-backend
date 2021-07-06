76414ab958f5371de8f3a4193e67d509c51cb1f9
NuBiFoRM <nubiform@me.com>
2021-07-06 22:09:17 +0900

    jwt secret

--- src/test/java/com/nubiform/order/config/security/jwt/JwtPropertiesTest.java
+++ src/test/java/com/nubiform/order/config/security/jwt/JwtPropertiesTest.java
@@ -5,12 +5,14 @@ import io.jsonwebtoken.io.Encoders;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class JwtPropertiesTest {
 
     public static final String SECRET = "Development|OrderManagementProject|com.nubiform|order-backend|8a79f833-6c60-4c3e-a5a5-6420136e1861";
